64cb255ecd7922ef3f110809b24ae314b5baa595
vboiko <valerafusion@gmail.com>
2018-04-03 16:06:20 +0300

    divisible_sum_pairs

--- hackerrank/algorithms/implementation/Divisible_Sum_Pairs.py
+++ hackerrank/algorithms/implementation/Divisible_Sum_Pairs.py
@@ -1,10 +1,10 @@
 def amount_of_pairs_for_index(index):
     global amount
-    for i in range(index + 1, len(arr)):
+    for i in range(index + 1, l):
         if (arr[i] + arr[index]) % k == 0: amount += 1
 
 amount = 0
-k = list(map(int, input().split()))[1]
+l, k = list(map(int, input().split()))
 arr = list(map(int, input().split()))
 
 for i, j in enumerate(arr): amount_of_pairs_for_index(i)
