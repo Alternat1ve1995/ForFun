e63dd88267c508b3ee6c28b93cb31a819c54d5e9
vboiko <dev.vboiko@gmail.com>
2018-06-08 23:40:43 +0300

    Update iota.py

--- telegram_bot/iota.py
+++ telegram_bot/iota.py
@@ -7,8 +7,10 @@ url = "https://api.telegram.org/bot" + token + "/"
 ids = []
 
 while True:
-    html_text = requests.get('https://prostocoin.com/c/iota').content
-    soup = BeautifulSoup(html_text)
-    price = soup.find('span', {'class': 'coinprice'}).text[1:] + ' $'
-    for id in ids: requests.post(url + 'sendMessage', data={'chat_id': id, 'text': price})
+    try:
+        html_text = requests.get('https://prostocoin.com/c/iota').content
+        soup = BeautifulSoup(html_text)
+        price = soup.find('span', {'class': 'coinprice'}).text[1:] + ' $'
+        for id in ids: requests.post(url + 'sendMessage', data={'chat_id': id, 'text': price})
+    except: pass
     sleep(3600)
